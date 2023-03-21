993c4eb1420f9a201dc817c3d85b9aa1171e2ff9
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-03-21 16:05:00 -0400

    remove * router

--- src/routes.js
+++ src/routes.js
@@ -42,6 +42,6 @@ export default function Router() {
                 { path: '*', element: <Navigate to='/404' /> }
             ]
         },
-        { path: '*', element: <Navigate to='/404' replace /> }
+        // { path: '*', element: <Navigate to='/404' replace /> }
     ])
 }
